10ff33c3882e2c8e9a19d255f2f4c4241406492c
Karish-15 <karish.ch15@gmail.com>
2022-02-11 19:56:49 +0530

    Added Home Page Tests

--- MessageBoard/posts/tests.py
+++ MessageBoard/posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.urls import reverse
+from .models import Post
+class PostModelTest(TestCase):
+    def setUp(self):    # Automatically called by django
+        Post.objects.create(text = "Just a test")
+        
+    def test_text_context(self): #Add test_ as prefix so django calls it automatically
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        
+        self.assertEqual(expected_object_name, 'Just a test')
+        
+class HomePageTest(TestCase):
+    
+    def setUp(self):
+        Post.objects.create(text = "another test")
+        
+    def test_home_route(self):
+        response = self.client.get(reverse('home'))     # reverse is equivalent of {{ url '' }} as in templates
+        self.assertEqual(200, response.status_code)
+        
+    def test_template_used(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(200, resp.status_code)
+        self.assertTemplateUsed(resp, 'home.html')
