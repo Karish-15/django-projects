7ed82a89dccdbf17b989656cf4a6c33c95362843
Karish-15 <karish.ch15@gmail.com>
2022-06-27 17:52:45 +0530

    Visit count

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -7,16 +7,18 @@ from django.views.generic import ListView, DetailView
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    
     num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
-    
     num_authors = Author.objects.all().count()
+    
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_authors': num_authors,
-        'num_instances_available': num_instances_available
+        'num_instances_available': num_instances_available,
+        'num_visits': num_visits,
     }
     
     return render(request, 'index.html', context=context)
